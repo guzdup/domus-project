49adbd1111b564f9089dcb03a7ff8fb9f0ea157c
Guzman Enrique <duguzpont@gmail.com>
2024-03-08 17:40:19 -0300

    cards are next to each other. find a way to have a space between them.

--- project/domus-project/src/App.jsx
+++ project/domus-project/src/App.jsx
@@ -2,6 +2,10 @@ import DomusHomeCard from './DomusHomeCard.jsx'
 import './App.css'
 
 function App() {
+
+    const formatPrice = (price) => `USD ${price}` 
+
+
     return(
         <section>
             <header className='domus-top'>
@@ -14,9 +18,24 @@ function App() {
                     Find your next home here!
                 </h1>
             </section>
-            <section>
-                <DomusHomeCard>
-                    
+            <section className='App'>
+                <DomusHomeCard
+                image='https://as1.ftcdn.net/v2/jpg/04/20/73/92/1000_F_420739259_YQkU0yMNGS8BZmssFDkEpHrgJAU0kxY9.jpg'
+                price='200.000'
+                formatPrice={formatPrice}
+                numberBedrooms={4}
+                numberBathrooms={3}
+                location={'Carrasco sur'}
+                >
+                </DomusHomeCard>
+                <DomusHomeCard
+                image='https://as1.ftcdn.net/v2/jpg/04/20/73/92/1000_F_420739259_YQkU0yMNGS8BZmssFDkEpHrgJAU0kxY9.jpg'
+                price='200.000'
+                formatPrice={formatPrice}
+                numberBedrooms={4}
+                numberBathrooms={3}
+                location={'Carrasco sur'}
+                >
                 </DomusHomeCard>
             </section>
             
--- project/domus-project/src/DomusHomeCard.jsx
+++ project/domus-project/src/DomusHomeCard.jsx
@@ -1,12 +1,27 @@
-import './App.css'
+import './DomusHomeCard.css'
 
-function DomusHomeCard({numberBedrooms, numberBathrooms, location}) {
+function DomusHomeCard({price, formatPrice, image, numberBedrooms, numberBathrooms, location}) {
     return(
         <div className="domus-homeCard">
-            <img src='' /> 
+            <img className='domus-homeCard-picture' src= {image} alt='houseImg'/> 
+            <strong className='domus-homeCard-price'>
+                {formatPrice(price)}
+            </strong>
             <div className='domus-homeCard-info'>
-                
+                <span className='domus-homeCard-bedrooms'>
+                    <img className='domus-homeCard-bedroomIcon' src="https://img.icons8.com/ios-glyphs/30/bed.png" alt="bed"/>
+                        {numberBedrooms}
+                </span>
+                <span className='domus-homeCard-bathrooms'>
+                    <img className='domus-homeCard-bathroomIcon' src='https://img.icons8.com/ios-filled/50/shower-and-tub.png' alt="shower-and-tub"/>
+                        {numberBathrooms}
+                </span>
             </div>
+            <span className='domus-homeCard-location'>
+                    <img className='domus-homeCard-locationIcon' src="https://img.icons8.com/ios-filled/50/marker.png" alt="marker"/>
+                        {location}
+                </span>
+            
         </div>
     )
 }

b28ec5b96f50666d02d752b4f204aa2f74129395
Guzman Enrique <duguzpont@gmail.com>
2024-03-09 17:20:09 -0300

    Fixed previous problems. Next -> image carrousel and improve UI

--- project/domus-project/src/App.jsx
+++ project/domus-project/src/App.jsx
@@ -1,8 +1,11 @@
 import DomusHomeCard from './DomusHomeCard.jsx'
 import './App.css'
+import home2 from './assets/images/home2.jpeg'
+import home3 from './assets/images/home3.jpeg'
+import home4 from './assets/images/home4.0.jpeg'
 
 function App() {
-
+    const home1 = 'https://as1.ftcdn.net/v2/jpg/04/20/73/92/1000_F_420739259_YQkU0yMNGS8BZmssFDkEpHrgJAU0kxY9.jpg'
     const formatPrice = (price) => `USD ${price}` 
 
 
@@ -18,23 +21,41 @@ function App() {
                     Find your next home here!
                 </h1>
             </section>
-            <section className='App'>
+            <section className='home-Section'>
                 <DomusHomeCard
-                image='https://as1.ftcdn.net/v2/jpg/04/20/73/92/1000_F_420739259_YQkU0yMNGS8BZmssFDkEpHrgJAU0kxY9.jpg'
+                image={home1}
                 price='200.000'
                 formatPrice={formatPrice}
                 numberBedrooms={4}
                 numberBathrooms={3}
+                location={'La Tahona'}
+                >
+                </DomusHomeCard>
+                <DomusHomeCard
+                image={home2}
+                price='190.000'
+                formatPrice={formatPrice}
+                numberBedrooms={4}
+                numberBathrooms={4}
+                location={'Jardines de Carrasco'}
+                >
+                </DomusHomeCard>
+                <DomusHomeCard
+                image={home3}
+                price='230.000'
+                formatPrice={formatPrice}
+                numberBedrooms={6}
+                numberBathrooms={5}
                 location={'Carrasco sur'}
                 >
                 </DomusHomeCard>
                 <DomusHomeCard
-                image='https://as1.ftcdn.net/v2/jpg/04/20/73/92/1000_F_420739259_YQkU0yMNGS8BZmssFDkEpHrgJAU0kxY9.jpg'
-                price='200.000'
+                image={home4}
+                price='193.000'
                 formatPrice={formatPrice}
-                numberBedrooms={4}
-                numberBathrooms={3}
-                location={'Carrasco sur'}
+                numberBedrooms={6}
+                numberBathrooms={5}
+                location={'Carrasco'}
                 >
                 </DomusHomeCard>
             </section>
